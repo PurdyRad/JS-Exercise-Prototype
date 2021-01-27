b338150bc3151656beac9cb0efb8e6e8d8d213de
Adam Purdy <purdyrad@Adams-MacBook-Air.local>
2021-01-27 17:01:13 -0500

    finished up mvp tasks

--- index.js
+++ index.js
@@ -39,14 +39,26 @@ function Airplane(name) {
           + It should return a string with `name` and `age`. Example: "Mary, 50"
   */
   
- function Person() {
+ function Person(name, age) {
+   this.name = name;
+   this.age = age;
+   this.stomach = [];
     
   }
  
+  Person.prototype.eat = function(edible){
+    if(this.stomach.length < 10){
+      this.stomach.push(edible);
+    }
+  }
  
-
+  Person.prototype.poop = function(){
+    this.stomach =[];
+  }
   
-  
+  Person.prototype.toString = function(string){
+    return `${this.name}, ${this.age}`
+  }
   
   
   /*
@@ -63,10 +75,18 @@ function Airplane(name) {
           + The `drive` method should return a string "I ran out of fuel at x miles!" x being `odometer`.
   */
   
- function Car() {
+ function Car(model, milesPerGallon) {
+   this.model = model,
+   this.tank = 0;
+   this.odometer = 0;
+   this.milesPerGallon = milesPerGallon
     
   }
   
+  Car.prototype.fill = function(gallons){
+    this.tank = this.tank + gallons
+
+  }
   
   /*
     TASK 3
@@ -75,18 +95,26 @@ function Airplane(name) {
       - Besides the methods on Person.prototype, babies have the ability to `.play()`:
           + Should return a string "Playing with x", x being the favorite toy.
   */
- function Baby() {
-   
+ function Baby(name, age, favoriteToy) {
+   this.name = name,
+   this.age = age,
+   this.favoriteToy = favoriteToy
   }
  
+  Baby.prototype = Object.create(Person.prototype)
+
+  Baby.prototype.play = function(){
+    return `Playing with ${this.favoriteToy}`
+  }
+  //Baby.prototype = Object.create(Person.prototype)
   
   /* 
     TASK 4
     In your own words explain the four principles for the "this" keyword below:
-    1. 
-    2. 
-    3. 
-    4. 
+    1. Global Binding-
+    2. Implicit Binding-
+    3. New Binding-
+    4. Explicit Binding-
   */
   
   
